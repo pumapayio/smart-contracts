957bd61983e301cbbb93a23c71878a67a2867310
Giorgos Kourtellos <giorgos@pumapay.io>
2019-10-16 12:14:53 +0300

    Add script for unifying smart contracts into one file
    The script unifies a smart contract into a single file to ease the deployment of the smart contract through Remix

--- scripts/unify.sh
+++ scripts/unify.sh
@@ -1,2 +1,31 @@
 #!/usr/bin/env bash
-node_modules/.bin/truffle-flattener contracts/topUp/TopUpPullPayment.sol >> unified/unified.sol
+
+# Exit script as soon as a command fails.
+set -o errexit
+
+read -p "Which smart contract you want to unify? " smartcontract
+
+read -p "Unified file name? " unifiedfname
+
+echo "Cleaning existing unified file unified/$unifiedfname.sol"
+echo '' > unified/$unifiedfname.sol
+echo "------------------------------"
+
+read -p "Is it located in a subdirectory in contracts  (y/n) ? " answer
+case ${answer:0:1} in
+y | Y)
+  read -p "Which subdirectory is it located? " subdirectory
+  echo "------------------------------"
+  echo "Smart contract in contracts subdir: $subdirectory/$smartcontract.sol --> unified/$unifiedfname.sol"
+  echo "------------------------------"
+  node_modules/.bin/truffle-flattener contracts/$subdirectory/$smartcontract.sol >> unified/$unifiedfname.sol
+  ;;
+*)
+  echo "------------------------------"
+  echo "Smart contract in contracts root dir contracts/$smartcontract --> unified/$unifiedfname.sol"
+  echo "------------------------------"
+  node_modules/.bin/truffle-flattener contracts/$smartcontract.sol >> unified/$unifiedfname.sol
+  ;;
+esac
+
+exit 0
